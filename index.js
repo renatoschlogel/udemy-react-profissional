12fd4e24db90b5c6fb8df15605ab2675255d9ca8
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-01-03 12:10:25 -0300

    Criando os primeiros componentes

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
+const App = () => (
+    <div className="container">
+        <NewNote/>
+        <NoteList/>
+    </div>
+);
+
+const NewNote = () => {
+    return (
+        <div className="new-note">
+            <input type="text" className="new-note__input"/>
+        </div>
+    );
+}
+
+const NoteList = () => (
+    <div className="node-list">
+        <div className="note">Teste</div>
+        <div className="note">Teste</div>
+        <div className="note">Teste</div>
+    </div>
+
+)
+
 ReactDOM.render(
-    <h1>E vamo que vamo!</h1>,
+    <App/>,
     document.getElementById("root")
 );
