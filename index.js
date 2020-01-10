8e1ecb34dfca5ebc6c10f5b61ae85fa542b6a1de
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-01-09 23:49:27 -0300

    Listas de chaves

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import classNames from "classnames";
+import uuid from "uuid/v1";
 
 import "./index.scss";
 
@@ -11,15 +13,34 @@ class App extends React.Component{
     
     handleAddNote = texto => {
         this.setState(prevState => ({
-            notes: prevState.notes.concat(texto)
+            notes: prevState.notes.concat({id: uuid(), text: texto  })
         }));
+    }
+
+    handleMove = (direction, index) =>{
+        
+        this.setState( prevState => {
+            
+            const newNotes = prevState.notes.slice();
+            const removedNote = newNotes.splice(index, 1)[0];
+
+            if (direction === "up") {
+                newNotes.splice(index - 1, 0, removedNote);
+            }else {
+                newNotes.splice(index + 1, 0, removedNote);
+            }
+
+            return {
+                notes: newNotes
+            };
+        })
     }
 
     render(){
         return (
             <div className="container">
                 <NewNote onAddNote={this.handleAddNote}/>
-                <NoteList notes={this.state.notes}/>
+                <NoteList notes={this.state.notes} onMove={this.handleMove}/>
             </div>
         );
     }
@@ -32,7 +53,7 @@ class NewNote extends React.Component {
     }
 
     render(){
-        const { onAddNote } = this.props;
+        const { onAddNote} = this.props;
         const { texto } = this.state;
 
         return (
@@ -61,10 +82,37 @@ class NewNote extends React.Component {
     }
 }
 
-const NoteList = ({notes}) => (
+const Icon = ({icon}) => {
+    return <i className="material-icons">{icon}</i>
+}
+
+const NoteList = ({notes, onMove}) => (
     <div className="node-list">
-        {notes.map(note => (
-           <div className="note">{note}</div>
+        {notes.map((note, index) => (
+           <div key={note.id} className="note">
+                <span className="note__text">{`${note.text}`}</span>
+                
+                <button className={classNames("note__button", {
+                    "note__button--hidden": index === 0
+                })} 
+                        onClick ={() => {
+                            onMove("up", index);
+                        }}
+                >
+                    <Icon icon="arrow_upward"/>
+                </button>
+                
+                <button className={classNames("note__button", {
+                    "note__button--hidden": index == notes.length - 1.
+                })}
+                            onClick ={() => {
+                            onMove("down", index);
+                        }}
+                    >
+                    <Icon icon="arrow_downward"/>
+                </button>
+                
+           </div>
          ))}
     </div>
 
